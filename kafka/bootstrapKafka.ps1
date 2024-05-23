113f03e403151926c65858837b9d5203127a9137
hopoffbaby <none>
2024-05-23 12:01:03 +0100

    update benchmark commands

--- kafka/bootstrapKafka.ps1
+++ kafka/bootstrapKafka.ps1
@@ -20,7 +20,10 @@ kubectl apply -f kafka-persistent-single.yaml -n kafka
 kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s -n kafka 
 
 # Run a Kafka benchmark producer to test the cluster
-kubectl -n kafka run kafka-benchmark-producer -ti --image=quay.io/strimzi/kafka:0.40.0-kafka-3.7.0 --rm=true --restart=Never -- bin/kafka-producer-perf-test.sh --topic my-topic --num-records 1000000 --record-size 400 --throughput 250000 --producer-props bootstrap.servers=my-cluster-kafka-bootstrap:9092
+kubectl -n kafka run kafka-benchmark-producer -ti --image=quay.io/strimzi/kafka:0.40.0-kafka-3.7.0 --rm=true --restart=Never -- bin/kafka-producer-perf-test.sh --topic my-topic --num-records 1000000 --record-size 400 --throughput -1 --producer-props bootstrap.servers=my-cluster-kafka-bootstrap:9092 compression.type=none batch.size=40000 linger.ms=1 partitioner.class=org.apache.kafka.clients.producer.RoundRobinPartitioner acks=1
+
+# consume some messages
+kubectl -n kafka run kafka-benchmark-producer -ti --image=quay.io/strimzi/kafka:0.40.0-kafka-3.7.0 --rm=true --restart=Never -- bin/kafka-consumer-perf-test.sh --topic my-topic --messages 100000 --bootstrap-server=bootstrap.servers=my-cluster-kafka-bootstrap:9092
 
 # Apply the Kafka rebalance configuration
 kubectl apply -f kafkaRebalance.yaml -n kafka
