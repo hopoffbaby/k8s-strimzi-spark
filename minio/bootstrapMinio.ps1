d86ce6515833cd2890cfc14e2ec19ff5062a2fb6
Neil Brown <nebrown@mercedesamgf1.com>
2024-07-17 11:57:26 +0100

    fix ilm rule

--- minio/bootstrapMinio.ps1
+++ minio/bootstrapMinio.ps1
@@ -77,7 +77,7 @@ kubectl exec -it pod/source-tenant-pool-0-0 -n minio-tenant-source -- /bin/sh -c
 
 #NOT WORKING - apparently mini time is 24h
 
-kubectl exec -it pod/source-tenant-pool-0-0 -n minio-tenant-source -- /bin/sh -c "mc ilm add --expire-delete-marker --noncurrent-expire-newer 0 --insecure myminio/test-bucket1"
+kubectl exec -it pod/source-tenant-pool-0-0 -n minio-tenant-source -- /bin/sh -c "mc ilm rule add --expire-delete-marker --noncurrent-expire-newer 0 --insecure myminio/test-bucket1"
 
 #can force a delete of non current versions using - BE VERY CAREFUL TO USE THIS COMMAND EXACTLY AS SHOWN - otherwise you can nuke all data
 #mc rm --insecure --versions --force --recursive --non-current myminio
